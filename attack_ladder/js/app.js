4bdbb063e114d49dc65401aa732915e6156dd67d
Ross Gore <ross.gore@gmail.com>
2023-06-09 09:03:43 -0400

    back to working i think
    back to working i think

--- attack_ladder/js/app.js
+++ attack_ladder/js/app.js
@@ -368,8 +368,6 @@ var refresh = function(){
 		 	else {
 		 		selectedPath = d;
 				console.log(selectedPath);
-				selectedPath.classed("selected", true)
-				.attr("class", "conn.hidden")
 		 	}
 
 		 	selectedNode = null;
