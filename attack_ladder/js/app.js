5bd417abb9c317ca3a42541cfba9e87d50723db2
Ross Gore <ross.gore@gmail.com>
2023-06-09 14:28:49 -0400

    hard coding hidden paths
    hard coding hidden paths

--- attack_ladder/js/app.js
+++ attack_ladder/js/app.js
@@ -337,14 +337,8 @@ var displayResources = function(){
 
 var refresh = function(){
 	
-	var visible_edges = [edges[0],edges[1], edges[2], edges[3], 
-	edges[4], edges[5], edges[6], edges[7], edges[8], edges[9],
-	edges[10], edges[11], edges[12], edges[13], edges[14], edges[15],
-	edges[16], edges[17]];
-	
-	
 	//data for the paths
-	paths = paths.data(visible_edges);
+	paths = paths.data(edges);
 	
 	
 	//update existing edges
@@ -383,7 +377,7 @@ var refresh = function(){
 		 })
 		 .on("contextmenu", d3.contextMenu(edgeMenu));
 	
-		 
+	paths[0].attr("class", "conn.hidden")
     //remove old paths
     paths.exit().remove();
 
